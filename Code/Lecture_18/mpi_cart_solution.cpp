2cae155e2a5f80bb84c3407bb2090433d97d2703
Eric Darve <darve@stanford.edu>
2020-03-06 08:47:54 -0800

    MPI Cart

---
+++ Code/Lecture_18/mpi_cart_solution.cpp
@@ -0,0 +1,55 @@
+#include "mpi.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    MPI_Init(&argc, &argv);
+
+    int num_procs, my_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    // We assume we have a simple 3D cubic grid
+    int q = static_cast<int>(cbrt(num_procs));
+    if (q * q * q != num_procs)
+    {
+        if (my_rank == 0)
+        {
+            cerr << "Number of processes must be a perfect cube." << endl;
+            cerr << "usage: mpirun -np <# procs> " << endl;
+        }
+        MPI_Finalize();
+        exit(1);
+    }
+
+    int dims[3] = {q, q, q};    // Dimension of 3D grid of processes
+    int periods[3] = {0, 0, 0}; // Whether or not our grid is periodic (wrap-around)
+
+    // Create a Cartesian topology
+
+    MPI_Comm comm_3d;
+
+    // int MPI_Cart_create(MPI_Comm comm_old, int ndims, const int dims[],
+    //                     const int periods[], int reorder, MPI_Comm * comm_cart)
+    MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 1, &comm_3d);
+
+    // int MPI_Comm_rank( MPI_Comm comm, int *rank )
+    int my3drank;
+    MPI_Comm_rank(comm_3d, &my3drank); // new rank in Cartesian grid
+
+    printf("[%1d] rank = %1d\n", my_rank, my3drank);
+
+    // MPI_Cart_coords(MPI_Comm comm, int rank, int maxdims, int coords[]
+    int coords[3];
+    MPI_Cart_coords(comm_3d, my3drank, 3, coords); // coordinates in topology
+
+    printf("[%1d] coords = (%1d, %1d, %1d)\n", my_rank, coords[0], coords[1], coords[2]);
+
+    MPI_Finalize();
+
+    return 0;
+}
--- Lecture Notes/Lecture_18/Lecture_18.md
+++ Lecture Notes/Lecture_18/Lecture_18.md
@@ -13,3 +13,82 @@ class: center, middle
 class: center, middle
 
 # Homework 5: matrix-matrix products
+
+---
+class: center, middle
+
+$$c\_{ij} = \sum\_{k=1}^n a\_{ik} b\_{kj}$$
+
+---
+class: center, middle
+
+![](2020-03-06-08-11-50.png)
+
+???
+
+Product is computed using block operations
+
+---
+class: center, middle
+
+# Process $(i,j,k)$
+
+![](2020-03-06-08-16-08.png)
+
+---
+class: center, middle
+
+# Data distribution
+
+Several steps are required
+
+Assume process 0 has matrix $A$ and $B$
+
+---
+class: middle
+
+1. Process 0 sends $a\_{ij}$ and $b\_{ij}$ to process $(i,j,0)$
+2. Process $(i,j,0)$ sends $a\_{ij}$ to $(i,j,j)$, and $b\_{ij}$ to $(i,j,i)$
+3. $(i,j,j)$ broadcasts $a\_{ij}$ to $(i,\*,j)$, and $(i,j,i)$ broadcasts $b\_{ij}$ to $(\*,j,i)$
+
+Process $(i,j,k)$ computes $a\_{ik} b\_{kj}$.
+
+---
+class: middle
+
+MPI functions that will be needed:
+
+1. That's a scatter. `MPI_Scatterv`
+2. That's a send/recv. `MPI_Send` and `MPI_Recv`
+3. That's a broadcast. `MPI_Bcast`
+
+---
+class: center, middle
+
+Scatter and broadcast are collective communications.
+
+But only a subset of processes are involved in the communication.
+
+---
+class: middle
+
+1. 0 &rarr; $(i,j,0)$
+2. P2P
+3. $(i,j,j)$ &rarr; $(i,\*,j)$, $(i,j,i)$ &rarr; $(\*,j,i)$
+
+---
+class: middle
+
+We will need two concepts:
+
+1. MPI topology
+2. MPI groups and communicators
+
+---
+class: center, middle
+
+# MPI topology
+
+We need a grid topology.
+
+Arranges all processes logically on a 3D grid $(i,j,k)$.
